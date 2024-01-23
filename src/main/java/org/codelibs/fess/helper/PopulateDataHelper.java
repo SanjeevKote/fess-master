99a6e0ab28f58e69b91f640dfe82b909678ce8e3
sanjeev kote <sanjeevkote.mca@gmail.com>
2024-01-23 11:58:23 +0530

    corrected

--- src/main/java/org/codelibs/fess/helper/PopulateDataHelper.java
+++ src/main/java/org/codelibs/fess/helper/PopulateDataHelper.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2023 CodeLibs Project and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.codelibs.fess.helper;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
@@ -41,150 +56,141 @@ import com.mongodb.client.gridfs.model.GridFSUploadOptions;
 import com.mongodb.client.model.InsertManyOptions;
 
 public class PopulateDataHelper {
-	private static final Logger logger = LogManager.getLogger(PopulateDataHelper.class);
+    private static final Logger logger = LogManager.getLogger(PopulateDataHelper.class);
 
-	private static MongoDatabase fessMongoDB;
-	private static MongoClient mongoClient;
+    private static MongoDatabase fessMongoDB;
+    private static MongoClient mongoClient;
 
-	private static void initializeProbes() {
-		try {
-			if (fessMongoDB == null) {
-				ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
-				CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
-				CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
+    private static void initializeProbes() {
+        try {
+            if (fessMongoDB == null) {
+                ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
+                CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+                CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
 
-				MongoClientSettings clientSettings =
-						MongoClientSettings.builder().applyConnectionString(connectionString).codecRegistry(codecRegistry).build();
-				mongoClient = MongoClients.create(clientSettings);
-				fessMongoDB = mongoClient.getDatabase("fessCrawlerDB");
-			}
-		} catch (Exception e) {
-			logger.error("Failed to load proprerty file {}.", e.getStackTrace());
-		}
-	}
+                MongoClientSettings clientSettings =
+                        MongoClientSettings.builder().applyConnectionString(connectionString).codecRegistry(codecRegistry).build();
+                mongoClient = MongoClients.create(clientSettings);
+                fessMongoDB = mongoClient.getDatabase("fessCrawlerDB");
+            }
+        } catch (Exception e) {
+            logger.error("Failed to load proprerty file {}.", e.getStackTrace());
+        }
+    }
 
-	private static void populateData(Object obj) {
-		initializeProbes();
-		MongoCollection<Document> gradesCollection = fessMongoDB.getCollection("fessDataCollection");
-		List<Document> grades = new ArrayList<>();
-		try {
-			grades.add(new Document("type", obj));
-			gradesCollection.insertMany(grades, new InsertManyOptions().ordered(false));
-		} catch (Exception e) {
-			logger.error("Failed to load proprerty file {}.", e.getStackTrace());
-		}
-		logger.info("Populated Object  Info: {} ", obj);
+    private static void populateData(Object obj) {
+        initializeProbes();
+        MongoCollection<Document> gradesCollection = fessMongoDB.getCollection("fessDataCollection");
+        List<Document> grades = new ArrayList<>();
+        try {
+            grades.add(new Document("type", obj));
+            gradesCollection.insertMany(grades, new InsertManyOptions().ordered(false));
+        } catch (Exception e) {
+            logger.error("Failed to load proprerty file {}.", e.getStackTrace());
+        }
+        logger.info("Populated Object  Info: {} ", obj);
 
-	}	
+    }
 
-	public static void populateFilesIntoDB(String fileSystemPath) throws IOException {
-		initializeProbes();
-		GridFSBucket gridFSBucket = GridFSBuckets.create(fessMongoDB);
-		try (Stream<Path> filePathStream=Files.walk(Paths.get(fileSystemPath))) {
-			filePathStream.forEach(filePath -> {
-				if (Files.isRegularFile(filePath)) {
+    public static void populateFilesIntoDB(String fileSystemPath) throws IOException {
+        initializeProbes();
+        GridFSBucket gridFSBucket = GridFSBuckets.create(fessMongoDB);
+        try (Stream<Path> filePathStream = Files.walk(Paths.get(fileSystemPath))) {
+            filePathStream.forEach(filePath -> {
+                if (Files.isRegularFile(filePath)) {
 
-//					System.out.println(filePath);
-					String fullFileName = filePath.toString();
+                    //					System.out.println(filePath);
+                    String fullFileName = filePath.toString();
 
-					if (fullFileName.contains(".pdf")) {
-						populatePDFFile(filePath, gridFSBucket);
+                    if (fullFileName.contains(".pdf")) {
+                        populatePDFFile(filePath, gridFSBucket);
 
-					} else if (fullFileName.contains(".doc")) {
-						populateDocFile(filePath, gridFSBucket);
-					} else {
-						populateTextFile(filePath, gridFSBucket);
-					}
+                    } else if (fullFileName.contains(".doc")) {
+                        populateDocFile(filePath, gridFSBucket);
+                    } else {
+                        populateTextFile(filePath, gridFSBucket);
+                    }
 
-				}
-			});
-		}
+                }
+            });
+        }
 
+    }
 
-	}
+    protected static void populatePDFFile(Path filePath, GridFSBucket gridFSBucket) {
+        try {
+            InputStream streamToUploadFrom = new FileInputStream(filePath.toString());
+            PDDocument pdfDoc;
+            pdfDoc = Loader.loadPDF(filePath.toFile());
+            PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+            stripper.setSortByPosition(true);
+            PDFTextStripper tStripper = new PDFTextStripper();
+            String pdfFileInText = tStripper.getText(pdfDoc);
+            //			System.out.println("The uploaded pdf file context: " + pdfFileInText);
 
-	protected static void populatePDFFile(Path filePath, GridFSBucket gridFSBucket) {
-		try {
-			InputStream streamToUploadFrom = new FileInputStream(filePath.toString());
-			PDDocument pdfDoc;
-			pdfDoc = Loader.loadPDF(filePath.toFile());
-			PDFTextStripperByArea stripper = new PDFTextStripperByArea();
-			stripper.setSortByPosition(true);
-			PDFTextStripper tStripper = new PDFTextStripper();
-			String pdfFileInText = tStripper.getText(pdfDoc);
-			//			System.out.println("The uploaded pdf file context: " + pdfFileInText);	
+            GridFSUploadOptions options = new GridFSUploadOptions().chunkSizeBytes(1024).metadata(new Document("path", pdfFileInText));
 
-			GridFSUploadOptions options = new GridFSUploadOptions()
-					.chunkSizeBytes(1024)
-					.metadata(new Document("path", pdfFileInText));
+            ObjectId fileId = gridFSBucket.uploadFromStream(filePath.toString(), streamToUploadFrom, options);
+            //			System.out.println("The fileId of the uploaded file is: " + fileId.toHexString());
+            logger.info("Populated Object  Info: {} ", fileId.toHexString());
+        } catch (FileNotFoundException e) {
+            logger.error("Failed to load proprerty file {}.", e.getStackTrace());
+        } catch (IOException e) {
+            logger.error("Failed to load proprerty file {}.", e.getStackTrace());
+        }
+    }
 
-			ObjectId fileId = gridFSBucket.uploadFromStream(filePath.toString(), streamToUploadFrom, options);
-			//			System.out.println("The fileId of the uploaded file is: " + fileId.toHexString());
-			logger.info("Populated Object  Info: {} ", fileId.toHexString());
-		} catch (FileNotFoundException e) {
-			logger.error("Failed to load proprerty file {}.", e.getStackTrace());
-		} catch (IOException e) {
-			logger.error("Failed to load proprerty file {}.", e.getStackTrace());
-		}
-	}
+    protected static void populateDocFile(Path filePath, GridFSBucket gridFSBucket) {
+        try {
 
+            InputStream streamToUploadFrom = new FileInputStream(filePath.toString());
+            FileInputStream fis;
 
-	protected static void populateDocFile(Path filePath, GridFSBucket gridFSBucket) {
-		try {
+            if (filePath.toString().substring(filePath.toString().length() - 1).equals("x")) { //is a docx
+                fis = new FileInputStream(filePath.toFile());
+                XWPFDocument doc = new XWPFDocument(fis);
+                XWPFWordExtractor extract = new XWPFWordExtractor(doc);
+                //					System.out.println(extract.getText());
+                //						 Create some custom options
+                GridFSUploadOptions options =
+                        new GridFSUploadOptions().chunkSizeBytes(1024).metadata(new Document("path", extract.getText()));
 
-			InputStream streamToUploadFrom = new FileInputStream(filePath.toString());
-			FileInputStream fis;
+                ObjectId fileId = gridFSBucket.uploadFromStream(filePath.toString(), streamToUploadFrom, options);
+                logger.info("Populated Object  Info: {} ", fileId.toHexString());
+            } else { //is not a docx
+                fis = new FileInputStream(filePath.toFile());
+                HWPFDocument doc = new HWPFDocument(fis);
+                WordExtractor extractor = new WordExtractor(doc);
+                //					System.out.println(extractor.getText());
+                //						 Create some custom options
+                GridFSUploadOptions options =
+                        new GridFSUploadOptions().chunkSizeBytes(1024).metadata(new Document("path", extractor.getText()));
 
-			if(filePath.toString().substring(filePath.toString().length() -1).equals("x")){ //is a docx
-				fis = new FileInputStream(filePath.toFile());
-				XWPFDocument doc = new XWPFDocument(fis);
-				XWPFWordExtractor extract = new XWPFWordExtractor(doc);
-				//					System.out.println(extract.getText());
-				//						 Create some custom options
-				GridFSUploadOptions options = new GridFSUploadOptions()
-						.chunkSizeBytes(1024)
-						.metadata(new Document("path", extract.getText()));
+                ObjectId fileId = gridFSBucket.uploadFromStream(filePath.toString(), streamToUploadFrom, options);
+                logger.info("Populated Object  Info: {} ", fileId.toHexString());
+            }
 
-				ObjectId fileId = gridFSBucket.uploadFromStream(filePath.toString(), streamToUploadFrom, options);
-				logger.info("Populated Object  Info: {} ", fileId.toHexString());
-			} else { //is not a docx
-				fis = new FileInputStream(filePath.toFile());
-				HWPFDocument doc = new HWPFDocument(fis);
-				WordExtractor extractor = new WordExtractor(doc);
-				//					System.out.println(extractor.getText());
-				//						 Create some custom options
-				GridFSUploadOptions options = new GridFSUploadOptions()
-						.chunkSizeBytes(1024)
-						.metadata(new Document("path", extractor.getText()));
+        } catch (FileNotFoundException e) {
+            logger.error("Failed to load proprerty file {}.", e.getStackTrace());
+        } catch (IOException e) {
+            logger.error("Failed to load proprerty file {}.", e.getStackTrace());
+        }
+    }
 
-				ObjectId fileId = gridFSBucket.uploadFromStream(filePath.toString(), streamToUploadFrom, options);
-				logger.info("Populated Object  Info: {} ", fileId.toHexString());
-			}
+    protected static void populateTextFile(Path filePath, GridFSBucket gridFSBucket) {
+        try {
+            InputStream streamToUploadFrom = new FileInputStream(filePath.toString());
+            String content;
+            content = Files.readString(filePath);
+            GridFSUploadOptions options = new GridFSUploadOptions().chunkSizeBytes(1024).metadata(new Document("path", content));
 
-		} catch (FileNotFoundException e) {
-			logger.error("Failed to load proprerty file {}.", e.getStackTrace());
-		} catch (IOException e) {
-			logger.error("Failed to load proprerty file {}.", e.getStackTrace());
-		}
-	}
-
-
-	protected static void populateTextFile(Path filePath, GridFSBucket gridFSBucket) {
-		try {
-			InputStream streamToUploadFrom = new FileInputStream(filePath.toString());
-			String content;
-			content = Files.readString(filePath);
-			GridFSUploadOptions options = new GridFSUploadOptions()
-					.chunkSizeBytes(1024)
-					.metadata(new Document("path", content));
-
-			ObjectId fileId = gridFSBucket.uploadFromStream(filePath.toString(), streamToUploadFrom, options);
-			logger.info("Populated Object  Info: {} ", fileId.toHexString());
-		} catch (FileNotFoundException e) {
-			logger.error("Failed to load proprerty file {}.", e.getStackTrace());
-		} catch (IOException e) {
-			logger.error("Failed to load proprerty file {}.", e.getStackTrace());
-		}
-	}
+            ObjectId fileId = gridFSBucket.uploadFromStream(filePath.toString(), streamToUploadFrom, options);
+            logger.info("Populated Object  Info: {} ", fileId.toHexString());
+        } catch (FileNotFoundException e) {
+            logger.error("Failed to load proprerty file {}.", e.getStackTrace());
+        } catch (IOException e) {
+            logger.error("Failed to load proprerty file {}.", e.getStackTrace());
+        }
+    }
 
 }
